a07fcdfce26304e42f2456260cc7dc6f809822c5
lamit47 <lamnuyen24062000@gmail.com>
2021-01-17 21:57:46 +0700

    LAM GIAO DIEN

---
+++ QuanLyQuanCaffe/frmQuanLyKho.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCaffe
+{
+    public partial class frmQuanLyKho : Form
+    {
+        public frmQuanLyKho()
+        {
+            InitializeComponent();
+        }
+
+        private void frmQuanLyKho_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
