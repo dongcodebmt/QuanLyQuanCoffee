ad9da6887e02352b11b1e6bca2e407d1ea84dd14
Tran Phuoc Anh <onlyway102@gmail.com>
2021-01-22 20:35:42 +0700

    Thêm QuanLyNguyenLieu (chua xong)

---
+++ Source Code/QuanLyQuanCoffee/frmQuanLyNguyenLieu.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace QuanLyQuanCaffe
+{
+    partial class frmQuanLyNguyenLieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtTenNL = new System.Windows.Forms.TextBox();
+            this.txtMaNL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(302, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(553, 349);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnHuy);
+            this.groupBox1.Controls.Add(this.btnLuu);
+            this.groupBox1.Controls.Add(this.btnSua);
+            this.groupBox1.Controls.Add(this.btnThem);
+            this.groupBox1.Controls.Add(this.txtTenNL);
+            this.groupBox1.Controls.Add(this.txtMaNL);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(32, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 269);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin nguyên liệu";
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(165, 205);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 23);
+            this.btnHuy.TabIndex = 5;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(16, 205);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 5;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(165, 160);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 5;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(16, 160);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // txtTenNL
+            // 
+            this.txtTenNL.Location = new System.Drawing.Point(141, 110);
+            this.txtTenNL.Name = "txtTenNL";
+            this.txtTenNL.Size = new System.Drawing.Size(100, 22);
+            this.txtTenNL.TabIndex = 4;
+            // 
+            // txtMaNL
+            // 
+            this.txtMaNL.Location = new System.Drawing.Point(141, 63);
+            this.txtMaNL.Name = "txtMaNL";
+            this.txtMaNL.Size = new System.Drawing.Size(100, 22);
+            this.txtMaNL.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên nguyên liệu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã nguyên liệu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(403, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quản lý thông tin nguyên liệu";
+            // 
+            // frmQuanLyNguyenLieu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 548);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmQuanLyNguyenLieu";
+            this.Text = "frmQuanLyNguyenLieu";
+            this.Load += new System.EventHandler(this.frmQuanLyNguyenLieu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtTenNL;
+        private System.Windows.Forms.TextBox txtMaNL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Source Code/QuanLyQuanCoffee/frmQuanLyNguyenLieu.cs
@@ -0,0 +1,78 @@
+﻿using QuanLyQuanCaffe.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCaffe
+{
+    public partial class frmQuanLyNguyenLieu : Form
+    {
+        QLCFContext model;
+
+        public frmQuanLyNguyenLieu()
+        {
+            model = new QLCFContext();
+            InitializeComponent();
+        }
+
+        private void frmQuanLyNguyenLieu_Load(object sender, EventArgs e)
+        {
+            model = new QLCFContext();
+            List<NguyenLieu> listNguyenLieu = model.NguyenLieu.ToList();
+            Setting();
+            BildingToDataGirdViewNguyenLieu(listNguyenLieu);
+            InitializeComponent();
+            ReadOnlyAll();
+            
+        }
+        private void BildingToDataGirdViewNguyenLieu(List<NguyenLieu> listNguyenLieu)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("STT");
+            dt.Columns.Add("Mã nguyên liệu");
+            dt.Columns.Add("Tên nguyên liệu");
+
+            int STT = 1;
+            for (int i = 0; i < listNguyenLieu.Count; i++, STT++)
+            {
+                NguyenLieu temp = listNguyenLieu[i];
+                dt.Rows.Add(new string[] { STT.ToString(), temp.ma.ToString(), temp.ten });
+            }
+            dataGridView1.DataSource = dt;
+        }
+        private void Setting()
+        {
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void ReadOnlyAll()
+        {
+            btnLuu.Visible = false;
+            btnHuy.Visible = false;
+            txtMaNL.Enabled = true;
+            txtTenNL.Enabled = true;
+        }
+        private void UnReadOnlyAll()
+        {
+            btnLuu.Visible = false;
+            btnHuy.Visible = false;
+
+        }
+        private void SetTextBoXNull()
+        {
+            txtMaNL.Text = "";
+            txtTenNL.Text = "";
+
+        }
+
+        
+    }
+}
+
+
+
